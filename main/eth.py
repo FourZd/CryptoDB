7c61409c19be99f6f75f13edd0d75da9b995c6fc
Nikolay Ryaskov <shogo.ryaskov@gmail.com>
2022-08-06 19:12:53 +0300

    Changed connection type

--- main/eth.py
+++ main/eth.py
@@ -19,9 +19,8 @@ from multiprocessing import Process
 import numpy
 import signal
 
-#YOU HAVE TO CONNECT TO LIGHTNODE FIRST $geth --syncmode light
-#IT WONT WORK OTHER WAY
-
+eth_provider = Web3.HTTPProvider('https://mainnet.infura.io/v3/6bc510d9c9664ba9bffd221e9dd7f666')
+w3_eth = Web3(eth_provider)
 class Timeout(): #simply timeout class, its neccesary in the future code
   """Timeout class using ALARM signal"""
   class Timeout(Exception): pass
@@ -52,7 +51,7 @@ class TxFromBlock: #  get TXs from the choosen block
 
     def get_tx_payload(self, current_block_number): # parsing TX addresses from block information and adds it to the set
         tx_payload = set() #set of unique transactions from the block
-        current_block = w3.eth.get_block(current_block_number) #get all block info
+        current_block = w3_eth.eth.get_block(current_block_number) #get all block info
         print('Transactions of block №', current_block_number, 'was successfully parsed')
         raw_tx_payload = current_block['transactions'] #get transactions from all info payload
         for elem in raw_tx_payload:
@@ -66,39 +65,40 @@ class TxCheck():
         pass
 
 
-    def server_call(self, tx_payload):
+    def server_call(self, tx_payload, block_number):
         for tx in tx_payload:
             try:
                 try:
                     with Timeout(1):
-                        tx_attributes = w3.eth.get_transaction_receipt(tx) #gets all info about transaction
+                        tx_attributes = w3_eth.eth.get_transaction_receipt(tx) #gets all info about transaction
                 except Timeout.Timeout:
-                    tx_attributes = w3.eth.get_transaction_receipt(tx) #if timeout bug, trying to get info again
+                    tx_attributes = w3_eth.eth.get_transaction_receipt(tx) #if timeout bug, trying to get info again
                 if tx_attributes['contractAddress'] != None: #contract creation transactions have contractAddress atr, so this is what we're looking for
                     print('Found!', tx_attributes['contractAddress'])
-                    self.append_to_db(tx_attributes['contractAddress']) 
+                    self.append_to_db(tx_attributes['contractAddress'], block_number) 
                         
-                else:
+                #else:
                     print('Not creation tx, keep searching...', '\n', 'From:', tx_attributes['from'], 'To:', tx_attributes['to'])
             except Exception as e: 
                 print(e, tx)
                 print('Transaction not found!')
             
 
-    def append_to_db(self, tx): #appending contract transactions to database
+    def append_to_db(self, tx, block_number): #appending contract transactions to database
         conn = sqlite3.connect(os.path.join(sys.path[0], 'CryptoDB'))
         cursor = conn.cursor()
         address = tx
-        append = cursor.execute(f"INSERT INTO ethcontracts (smart_contract) VALUES(?)", (address,))
+        append = cursor.execute(f"INSERT INTO smartcontracts (contract, blockchain, block_number) VALUES(?, ?, ?)", (address, 'eth', block_number))
         conn.commit()
+        conn.close()
 
 
-    def main(self, block_payload): #main script, creating 12 proccesses for fast-working(async doesnt work with web3 somewhy)
+    def main(self, block_payload, block_number): #main script, creating 12 proccesses for fast-working(async doesnt work with web3 somewhy)
         proccesses = []
         start_time = time()
         l = numpy.array_split(numpy.array(block_payload), 12)
         for array in l:
-            proc = Process(target=self.server_call, args=(array, ))
+            proc = Process(target=self.server_call, args=(array, block_number, ))
             proccesses.append(proc)
         for p in proccesses:
             try:
@@ -118,14 +118,16 @@ class DBCommunnication():
 
 
     def __init__(self):
-        self.conn = sqlite3.connect(os.path.join(sys.path[0], 'CryptoDB'))
-        self.cursor = self.conn.cursor()
-    
+        pass
+
 
     def get_last_block(self): #getting last saved block from the database, if theres none - asking if user want to create default(last) value
+        conn = sqlite3.connect(os.path.join(sys.path[0], 'CryptoDB'))
+        cursor = conn.cursor()
         try:
-            query = self.cursor.execute('SELECT number FROM proccessedblocks WHERE rowid == 1')
+            query = cursor.execute('SELECT number FROM proccessedblocks WHERE blockchain =?', ('eth',))
             result = query.fetchone()
+            conn.close()
             return result[0]
         except TypeError as e:
             print('It looks like you did not set neither desired block number nor default')
@@ -134,15 +136,17 @@ class DBCommunnication():
                 desired_or_default = int(input('Do you want to set desired number, or set by default(last existing)? 1/2'))
                 if desired_or_default == 1:
                     desired_number_set = int(input('Write the desired block number'))
-                    change_number_to_desired = self.cursor.execute(f"INSERT INTO proccessedblocks values (1, {desired_number_set})")
-                    self.conn.commit()
+                    change_number_to_desired = cursor.execute(f"INSERT INTO proccessedblocks values (1, {desired_number_set})")
+                    conn.commit()
+                    conn.close()
                     print('Your number was successfully changed to desired:', desired_number_set)
                     return desired_number_set
                 elif desired_or_default == 2:
                     print('Setting current block as default(last exists)')
-                    default_block_num = w3.eth.get_block('latest')['number']
+                    default_block_num = w3_eth.eth.get_block('latest')['number']
                     change_number_to_default = self.cursor.execute(f"INSERT INTO proccessedblocks values (1, {default_block_num})")
-                    self.conn.commit()
+                    conn.commit()
+                    conn.close()
                     return default_block_num
                 else:
                     raise Exception('Invalid input')
@@ -155,9 +159,12 @@ class DBCommunnication():
 
 
     def update_last_block(self, completed_block_number): #after one cycle, adding next block number to DB
+        conn = sqlite3.connect(os.path.join(sys.path[0], 'CryptoDB'))
+        cursor = conn.cursor()
         next_block_number_to_use = completed_block_number + 1
-        change_number_to_default = self.cursor.execute(f"REPLACE INTO proccessedblocks values (1, {next_block_number_to_use})")
-        self.conn.commit()
+        change_number_to_default = cursor.execute(f"REPLACE INTO proccessedblocks VALUES(?, ?, ?)", (1, 'eth', next_block_number_to_use,))
+        conn.commit()
+        conn.close()
         print('Next block to use: №', next_block_number_to_use)
 
 class MainProccess(): #just a starter for script cycle
@@ -172,13 +179,12 @@ class MainProccess(): #just a starter for script cycle
 
 
     def start_work(self):
-        while True: #theres no reason to stop this cycle
+        while True:
             self.number_to_parse = self.dbcommunication.get_last_block() #last block number from DB
             self.tx_payload = list(self.tx_from_block.get_tx_payload(self.number_to_parse)) #parsing txs from block
-            self.tx_checker.main(self.tx_payload) #checking if tx creates contract
+            self.tx_checker.main(self.tx_payload, self.number_to_parse) #checking if tx creates contract
             self.dbcommunication.update_last_block(self.number_to_parse) #adding next block to the DB
 
 
 
-main = MainProccess()
-main.start_work()
+eth_main = MainProccess()
