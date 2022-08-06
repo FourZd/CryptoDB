cf829a76a4b43081ee1ca16aebc693019e316da4
Nikolay Ryaskov <shogo.ryaskov@gmail.com>
2022-08-06 16:28:34 +0300

    added bsc chain to DB [1]

--- main/database_conn.py
+++ main/database_conn.py
@@ -1,16 +1,22 @@
 from typing import Text
 from peewee import *
-from web3.auto import w3
+from web3.auto import w3, Web3
+from web3.middleware import geth_poa_middleware
 
+bsc_provider = Web3.HTTPProvider('https://bsc-dataseed1.binance.org/')
+w3_bsc = Web3(bsc_provider)
+w3_bsc.middleware_onion.inject(geth_poa_middleware, layer=0)
 CryptoDB = SqliteDatabase('/home/fourzd/CryptoDB/main/CryptoDB')
 
 class ProccessedBlocks(Model):
-    id = BigAutoField()
+    blockchain = TextField()
     number = IntegerField()
     
 
     class Meta:
         database = CryptoDB 
+
+
 class EthContracts(Model):
     id = BigAutoField()
     smart_contract = TextField()
@@ -18,6 +24,17 @@ class EthContracts(Model):
 
     class Meta:
         database = CryptoDB
+
+
+class BscContracts(Model):
+    id = BigAutoField()
+    smart_contract = TextField()
+
+
+    class Meta:
+        database = CryptoDB
+
+
 class DBOrganization():
   
     def __init__(self):
@@ -28,21 +45,40 @@ class DBOrganization():
         CryptoDB.connect()
         CryptoDB.create_tables([ProccessedBlocks], safe = True)
         CryptoDB.create_tables([EthContracts], safe = True)
+        CryptoDB.create_tables([BscContracts], safe = True)
         CryptoDB.close()
     
 
     def default_block(self): #if there's no information in the DB, set a number of the last block
+        #id 1 = ETH
+        #id 2 = BSC
         try:
-            ProccessedBlocks.get_by_id(1) != 0
+            ProccessedBlocks.get_by_id(1) 
         except DoesNotExist:
-            default_block = ProccessedBlocks.insert(id=1, number=w3.eth.get_block('latest')['number']).execute()
+            default_block = ProccessedBlocks.insert(blockchain = 'eth', number=w3.eth.get_block('latest')['number']).execute()
         else:
             print('Block already exists')
-            restart_call = input('Do you want to reset it to the last existing block? Y/n')
+            restart_call = input('Do you want to reset it to the last existing eth block? Y/n')
             if restart_call == 'Y':
                 res = (ProccessedBlocks
                 .update(number=w3.eth.get_block('latest')['number'])
-                .where(ProccessedBlocks.id == '1')
+                .where(ProccessedBlocks.blockchain == 'eth')
+                .execute())
+            elif restart_call == 'n':
+                print('Restart call skipped')
+            else:
+                raise Exception('Wrong input')
+        try:
+            ProccessedBlocks.get_by_id(2)
+        except DoesNotExist:
+            default_block = ProccessedBlocks.insert(blockchain = 'bsc', number=w3_bsc.eth.get_block('latest')['number']).execute()
+        else:
+            print('Block already exists')
+            restart_call = input('Do you want to reset it to the last existing bsc block? Y/n')
+            if restart_call == 'Y':
+                res = (ProccessedBlocks
+                .update(number=w3_bsc.eth.get_block('latest')['number'])
+                .where(ProccessedBlocks.blockchain == 'bsc')
                 .execute())
             elif restart_call == 'n':
                 print('Restart call skipped')
