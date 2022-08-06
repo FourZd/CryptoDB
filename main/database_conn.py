6bac3d9053fef66148e562c82bd7a90438382b40
Nikolay Ryaskov <shogo.ryaskov@gmail.com>
2022-08-06 19:12:20 +0300

    Updated for bsc chain

--- main/database_conn.py
+++ main/database_conn.py
@@ -6,6 +6,10 @@ from web3.middleware import geth_poa_middleware
 bsc_provider = Web3.HTTPProvider('https://bsc-dataseed1.binance.org/')
 w3_bsc = Web3(bsc_provider)
 w3_bsc.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+eth_provider = Web3.HTTPProvider('https://mainnet.infura.io/v3/6bc510d9c9664ba9bffd221e9dd7f666')
+w3_eth = Web3(eth_provider)
+
 CryptoDB = SqliteDatabase('/home/fourzd/CryptoDB/main/CryptoDB')
 
 class ProccessedBlocks(Model):
@@ -17,18 +21,11 @@ class ProccessedBlocks(Model):
         database = CryptoDB 
 
 
-class EthContracts(Model):
+class SmartContracts(Model):
     id = BigAutoField()
-    smart_contract = TextField()
-
-
-    class Meta:
-        database = CryptoDB
-
-
-class BscContracts(Model):
-    id = BigAutoField()
-    smart_contract = TextField()
+    contract = TextField()
+    blockchain = TextField()
+    block_number = IntegerField()
 
 
     class Meta:
@@ -44,8 +41,7 @@ class DBOrganization():
     def initialize_db(self):
         CryptoDB.connect()
         CryptoDB.create_tables([ProccessedBlocks], safe = True)
-        CryptoDB.create_tables([EthContracts], safe = True)
-        CryptoDB.create_tables([BscContracts], safe = True)
+        CryptoDB.create_tables([SmartContracts], safe = True)
         CryptoDB.close()
     
 
@@ -55,13 +51,13 @@ class DBOrganization():
         try:
             ProccessedBlocks.get_by_id(1) 
         except DoesNotExist:
-            default_block = ProccessedBlocks.insert(blockchain = 'eth', number=w3.eth.get_block('latest')['number']).execute()
+            default_block = ProccessedBlocks.insert(blockchain = 'eth', number=w3_eth.eth.get_block('latest')['number']).execute()
         else:
             print('Block already exists')
             restart_call = input('Do you want to reset it to the last existing eth block? Y/n')
             if restart_call == 'Y':
                 res = (ProccessedBlocks
-                .update(number=w3.eth.get_block('latest')['number'])
+                .update(number=w3_eth.eth.get_block('latest')['number'])
                 .where(ProccessedBlocks.blockchain == 'eth')
                 .execute())
             elif restart_call == 'n':
