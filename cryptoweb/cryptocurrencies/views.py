c19e1ef5c053b765850ef4b51e14e0a17db2a731
Nikolay Ryaskov <shogo.ryaskov@gmail.com>
2022-08-29 21:57:43 +0300

    Rendering foundation

--- cryptoweb/cryptocurrencies/views.py
+++ cryptoweb/cryptocurrencies/views.py
@@ -2,8 +2,12 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blockchain, SmartContract
 
 def main_page(request):
     return HttpResponse('<h1> You are at the main page </h1>')
 def contracts(request):
-    return HttpResponse('Testing')
+    contracts = SmartContract.objects.all()
+    return render(request, 'cryptocurrencies/contracts.html', {
+        'contracts': contracts, 'title': 'List of contracts'
+        })
